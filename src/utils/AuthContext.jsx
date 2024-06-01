f8db90248df454e3cba7a96bbd2e69857598a82f
shettykirthan <nnm22ad051@nmamit.in>
2024-06-01 13:13:21 +0530

    updated

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,107 +1,136 @@
-    import { useContext, useState, useEffect, createContext } from "react";
-    import React from 'react';
-    import { account, databases } from "../appwrite/config";
+import { useContext, useState, useEffect, createContext } from "react";
+import React from 'react';
+import { account, databases } from "../appwrite/config";
 
-    const AuthContext = createContext();
+const AuthContext = createContext();
 
-    export const AuthProvider = ({ children }) => {
-        const [loading, setLoading] = useState(true);
-        const [user, setUser] = useState(null);
+const Loader = () => {
+  const loaderStyle = {
+    border: '10px solid #f3f3f3', /* Light grey */
+    borderTop: '10px solid #3498db', /* Blue */
+    borderRadius: '50%',
+    width: '90px',
+    height: '90px',
+    animation: 'spin 1s linear infinite'
+  };
+  const loaderContainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh' // Full viewport height to center vertically
+  };
 
-        useEffect(() => {
-            checkUserStatus();
-        }, []);
+  return (
+    <div style={loaderContainerStyle}>
+      <div style={loaderStyle}></div>
+      <style>
+        {`
+          @keyframes spin {
+            0% { transform: rotate(0deg); }
+            100% { transform: rotate(360deg); }
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
-        const checkUserStatus = async () => {
-            try {
-                const currentUser = await account.get();
-                setUser(currentUser);
-            } catch (e) {
-                console.error("No active session", e);
-            }
-            setLoading(false);
-        };
+  useEffect(() => {
+    checkUserStatus();
+  }, []);
 
-        const loginUser = async (userInfo) => {
-            setLoading(true);
-            try {
-                await account.createEmailPasswordSession(
-                    userInfo.email,
-                    userInfo.password
-                );
-                const currentUser = await account.get();
-                setUser(currentUser);
-            } catch (e) {
-                console.error(e);
-            }
-            setLoading(false);
-        };
+  const checkUserStatus = async () => {
+    try {
+      const currentUser = await account.get();
+      setUser(currentUser);
+    } catch (e) {
+      console.error("No active session", e);
+    }
+    setLoading(false);
+  };
 
-        const logoutUser = async () => {
-            setLoading(true);
-            try {
-                await account.deleteSession('current');
-                setUser(null);
-            } catch (e) {
-                console.error(e);
-            }
-            setLoading(false);
-        };
+  const loginUser = async (userInfo) => {
+    setLoading(true);
+    try {
+      await account.createEmailPasswordSession(
+        userInfo.email,
+        userInfo.password
+      );
+      const currentUser = await account.get();
+      setUser(currentUser);
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
 
-        const registerUser = (userInfo) => {
-            // Registration logic here
-        };
+  const logoutUser = async () => {
+    setLoading(true);
+    try {
+      await account.deleteSession('current');
+      setUser(null);
+    } catch (e) {
+      console.error(e);
+    }
+    setLoading(false);
+  };
 
-        const addDocument = async (documentData, documentId = null) => {
-            if (!user) {
-                throw new Error("User is not authenticated");
-            }
-        
-            console.log("Adding document with data:", documentData); // Log document data
-        
-            try {
-                let response;
-                if (documentId) {
-                    response = await databases.updateDocument(
-                        '665999ea0028d0f501a9', // Replace with your database ID
-                        '6659a5ae0027611de231', // Replace with your collection ID
-                        documentId, // Document ID to update
-                        documentData
-                    );
-                } else {
-                    response = await databases.createDocument(
-                        '665999ea0028d0f501a9', // Replace with your database ID
-                        '6659a5ae0027611de231', // Replace with your collection ID
-                        'unique()', // Unique ID for the document
-                        documentData
-                    );
-                }
-                console.log("Document response:", response);
-                return response;
-            } catch (error) {
-                console.error("Error adding document:", error);
-                throw error; // Ensure error is thrown to be caught in the component
-            }
-        };
-        
+  const registerUser = (userInfo) => {
+    // Registration logic here
+  };
 
-        const contextData = {
-            user,
-            loginUser,
-            logoutUser,
-            registerUser,
-            addDocument,
-        };
+  const addDocument = async (documentData, documentId = null) => {
+    if (!user) {
+      throw new Error("User is not authenticated");
+    }
 
-        return (
-            <AuthContext.Provider value={contextData}>
-                {loading ? <p>Loading...</p> : children}
-            </AuthContext.Provider>
+    console.log("Adding document with data:", documentData); // Log document data
+
+    try {
+      let response;
+      if (documentId) {
+        response = await databases.updateDocument(
+          '665999ea0028d0f501a9', // Replace with your database ID
+          '6659a5ae0027611de231', // Replace with your collection ID
+          documentId, // Document ID to update
+          documentData
         );
-    };
+      } else {
+        response = await databases.createDocument(
+          '665999ea0028d0f501a9', // Replace with your database ID
+          '6659a5ae0027611de231', // Replace with your collection ID
+          'unique()', // Unique ID for the document
+          documentData
+        );
+      }
+      console.log("Document response:", response);
+      return response;
+    } catch (error) {
+      console.error("Error adding document:", error);
+      throw error; // Ensure error is thrown to be caught in the component
+    }
+  };
 
-    export const useAuth = () => {
-        return useContext(AuthContext);
-    };
+  const contextData = {
+    user,
+    loginUser,
+    logoutUser,
+    registerUser,
+    addDocument,
+  };
 
-    export default AuthContext;
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? <Loader /> : children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthContext;
